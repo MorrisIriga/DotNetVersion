ccad25e4b495b34dda274dafc0077197395b4555
morrisiriga <muthuimorris@ymail.com>
2016-02-29 18:53:55 +0300

    Initial version.

---
+++ DotNetVersion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace DotNetVersion
+{
+    class Program
+    {
+
+        /* Main */
+        static void Main(string[] args)
+        {
+            RegistryKey myKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP");
+
+            if (myKey == null)
+            {
+                // If key is null, then there was an error retrieving the key
+                Console.WriteLine("Error retrieving registry key!\n");
+                return;
+            }
+            else
+            {
+                Boolean subkeyV4Exists = false;
+                foreach (string subkey in myKey.GetSubKeyNames())
+                {
+                    if (subkey == "v4")
+                    {
+                        subkeyV4Exists = true;
+                        break;
+                    }
+                }
+
+                if (!subkeyV4Exists)
+                {
+                    myKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\Full");
+                    Console.WriteLine(".NET Framework Version: " + myKey.GetValue("Version") + "\n");
+                }
+                else
+                {
+                    myKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full");
+                    Console.WriteLine(".NET Framework Version: " + myKey.GetValue("Release") + "\n");
+                }
+            }
+
+        }
+
+    }
+}
